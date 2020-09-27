7f627849f2e8d6feb07d9aff153e2b28bbe1490c
hoh miyazawa <hohMiyazawa@yandex.com>
2020-09-27 10:03:17 +0200

    argument parsing

--- bsqip.c
+++ bsqip.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 void printUsage(){
 	printf("bsqip [options] -e -i infile.svg -o outfile.bsqip\n");
@@ -25,6 +27,72 @@ int main(int argc,char *argv[]){
 	){
 		printUsage();
 		return 1;
+	}
+	bool hasD = false;
+	bool hasE = false;
+	bool hasI = false;
+	bool hasO = false;
+	bool hasC = false;
+	bool hasM = false;
+	char *infile;
+	char *outfile;
+	int colourType = 2;//default truecolour
+	int shapeType  = 0;//default triangles
+	for(int i=1;i<argc;i++){
+		if(strcmp("-e",argv[i]) == 0){
+			if(hasD || hasE){
+				printUsage();
+				return 2;
+			}
+			hasE = true;
+		}
+		else if(strcmp("-d",argv[i]) == 0){
+			if(hasD || hasE){
+				printUsage();
+				return 2;
+			}
+			hasD = true;
+		}
+		else if(strcmp("-c",argv[i]) == 0){
+			i++;
+			if(hasD || hasC || i == argc){
+				printUsage();
+				return 2;
+			}
+			hasC = true;
+			colourType = atoi(argv[i]);
+		}
+		else if(strcmp("-m",argv[i]) == 0){
+			i++;
+			if(hasD || hasM || i == argc){
+				printUsage();
+				return 2;
+			}
+			hasM = true;
+			shapeType = atoi(argv[i]);
+		}
+		else if(strcmp("-i",argv[i]) == 0){
+			i++;
+			if(hasI || i == argc){
+				printUsage();
+				return 2;
+			}
+			hasI = true;
+			infile = argv[i];
+		}
+		else if(strcmp("-o",argv[i]) == 0){
+			i++;
+			if(hasO || i == argc){
+				printUsage();
+				return 2;
+			}
+			hasO = true;
+			outfile = argv[i];
+		}
+		else{
+			printUsage();
+			return 2;
+		}
 	}
 	
 	return 0;
